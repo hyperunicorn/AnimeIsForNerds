9e864010b787053546ded542fcd83c491f6ec245
Valis <hyperunicorn@users.noreply.github.com>
2023-03-25 16:06:21 +1100

    Update ScrapeComments.js

--- ScrapeComments.js
+++ ScrapeComments.js
@@ -6,9 +6,9 @@ function scrape(){
   ix = 0;
   while (ix < chatWindow.length){
     chat = chatWindow[ix];
-    tm = ch.children[1].children[0].innerText;
-    content = ch.children[1].children[2].innerText;
-    user = ch.children[1].children[1].children[1].innerText;
+    tm = chat.children[1].children[0].innerText;
+    content = chat.children[1].children[2].innerText;
+    user = chat.children[1].children[1].children[1].innerText;
     msg = {"time" : tm, "content" : content, "user" : user};
     msgs.push(msg);
     ix += 1;
